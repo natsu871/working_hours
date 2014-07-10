396ecd4c1b435f14b05ef1b8f47f03d32a2ff27e
Natsumi Hirano <rubymydear.n@gmail.com>
2014-07-10 15:31:37 +0900

    Add records controller

---
+++ app/controllers/records_controller.rb
@@ -0,0 +1,39 @@
+class RecordsController < ApplicationController
+  def index
+    @records = Record.all
+  end
+
+  def new
+    @record = Record.where('recorded_on = ?', Date.today).first || Record.new
+  end
+
+  def create
+    @record = Record.new(record_params)
+    if @record.save
+      flash[:success] = 'Created'
+      redirect_to records_path
+    else
+      render action: :index
+    end
+  end
+
+  def edit
+    @record = Record.find(params[:id])
+  end
+
+  def update
+    @record = Record.find(params[:id])
+    if @record.update(record_params)
+      flash[:success] = 'Updated'
+      redirect_to records_path
+    else
+      render action: :edit
+    end
+  end
+
+  private
+
+  def record_params
+    params.require(:record).permit(:recorded_on, :started_at, :ended_at)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,16 @@
 Rails.application.routes.draw do
   devise_for :users
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
+  resources :records
 
-  # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'records#new'
+
+  # The priority is based upon order of creation: first created -> highest priority.
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
-
-  # Example resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
 
   # Example resource route with options:
   #   resources :products do
